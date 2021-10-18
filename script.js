ae1571e3fd0942fbe6c31f5c88a839ddfbad92dc
hjkwon0507 <88071843+hjkwon0507@users.noreply.github.com>
2021-10-18 16:24:50 +0900

    file upload js

--- script.js
+++ script.js
@@ -1,32 +1,16 @@
-window.onload = function(){
-    slideOne();
-    slideTwo();
-}
+function selectedVideo(self) {
+    var file = self.files[0];
+    var reader = new FileReader();
 
-let sliderOne = document.getElementById("slider-1");
-let sliderTwo = document.getElementById("slider-2");
-let displayValOne = document.getElementById("range1");
-let displayValTwo = document.getElementById("range2");
-let minGap = 0;
-let sliderTrack = document.querySelector(".slider-track");
-let sliderMaxValue = document.getElementById("slider-1").max;
+    reader.onload = function (e) {
+        var src = e.target.result;
+        var video = document.getElementById("video");
+        var source = document.getElementById("source");
 
-function slideOne(){
-    if(parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap){
-        sliderOne.value = parseInt(sliderTwo.value) - minGap;
-    }
-    displayValOne.textContent = sliderOne.value;
-    fillColor();
-}
-function slideTwo(){
-    if(parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap){
-        sliderTwo.value = parseInt(sliderOne.value) + minGap;
-    }
-    displayValTwo.textContent = sliderTwo.value;
-    fillColor();
-}
-function fillColor(){
-    percent1 = (sliderOne.value / sliderMaxValue) * 100;
-    percent2 = (sliderTwo.value / sliderMaxValue) * 100;
-    sliderTrack.style.background = `linear-gradient(to right, #dadae5 ${percent1}% , #3264fe ${percent1}% , #3264fe ${percent2}%, #dadae5 ${percent2}%)`;
+        source.setAttribute("src", src);
+        video.load();
+        video.onplay();
+    };
+
+    reader.readAsDataURL(file)
 }
